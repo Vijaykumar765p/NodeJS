367eebd3e699f696f717eeefad3c11beeae1af4c
Vijaykumar765p <vpattar6@gmail.com>
2018-08-27 18:17:13 +0530

    27-8

--- platforms/browser/www/js/transformations.js
+++ platforms/browser/www/js/transformations.js
@@ -1,35 +1,41 @@
 'use strict';
- 
 
- function loadtransData(){
-  
- try{
-	var http = new XMLHttpRequest();
-	http.open("GET", _TR_DETAIL_URL , true );
-	//http.setRequestHeader("Authorization", "Basic Zml0bW9zaGE6V2Vya0l0R2lybDA4MTAh");
-	//http.setRequestHeader("Content-type", "application/json");
-		http.onreadystatechange = function() {
-			if(http.readyState == 4 && http.status == 200) {
+
+function loadtransData() {
+
+	try {
+		var http = new XMLHttpRequest();
+		http.open("GET", _TR_DETAIL_URL, true);
+		//http.setRequestHeader("Authorization", "Basic Zml0bW9zaGE6V2Vya0l0R2lybDA4MTAh");
+		//http.setRequestHeader("Content-type", "application/json");
+		http.onreadystatechange = function () {
+			if (http.readyState == 4 && http.status == 200) {
 				//console.log( JSON.parse( http.responseText ).fitness_level );
-			  //localStorage.setItem( "USER_INFO" ,  http.responseText  );
-			  transformations =   JSON.parse( Base64.decode(  http.responseText ) );
-			  
-			  localStorage.setItem("transformations" , JSON.stringify(transformations) );
-			  try{	len = transformations[0].details.length;
-			  console.log(transformations[0].details.length);    }catch(err){ }
-			  
-			  
-			} 
+				//localStorage.setItem( "USER_INFO" ,  http.responseText  );
+				transformations = JSON.parse(Base64.decode(http.responseText));
 
-			http.onerror = function(error){
-				transformations = JSON.parse(localStorage.getItem( "transformations" ) || "[]" );		
-				try{	len = transformations[0].details.length;    }catch(err){ }
- 
-				 	 
+				localStorage.setItem("transformations", JSON.stringify(transformations));
+				try {
+					len = transformations[0].details.length;
+					console.log(transformations[0].details.length);
+				} catch (err) {}
+
+
 			}
+
+			http.onerror = function (error) {
+				transformations = JSON.parse(localStorage.getItem("transformations") || "[]");
+				try {
+					len = transformations[0].details.length;
+				} catch (err) {}
+
+
+			}
+		}
+		http.send();
+	} catch (err) {
+		alert(err);
 	}
-	http.send();
-}catch(err){alert(err);}	
 }
- 
- loadtransData();
+
+loadtransData();
--- www/js/properties.js
+++ www/js/properties.js
@@ -1,40 +1,25 @@
 "use strict";
 
-
-
- function loadPropertiesData(){
-
- 
- try{
-	var http = new XMLHttpRequest();
-	http.open("GET", _VD_LOCK_DETAIL_URL , true );
-	//http.setRequestHeader("Authorization", "Basic Zml0bW9zaGE6V2Vya0l0R2lybDA4MTAh");
-	//http.setRequestHeader("Content-type", "application/json");
-		http.onreadystatechange = function() {
-			if(http.readyState == 4 && http.status == 200) {
+function loadPropertiesData() {
+	try {
+		var http = new XMLHttpRequest();
+		http.open("GET", _VD_LOCK_DETAIL_URL, true);
+		//http.setRequestHeader("Authorization", "Basic Zml0bW9zaGE6V2Vya0l0R2lybDA4MTAh");
+		//http.setRequestHeader("Content-type", "application/json");
+		http.onreadystatechange = function () {
+			if (http.readyState == 4 && http.status == 200) {
 				//console.log( JSON.parse( http.responseText ).fitness_level );
-			 VIDEO_CONFIG = JSON.parse( Base64.decode( http.responseText ) );
-			  localStorage.setItem("properties" , JSON.stringify(VIDEO_CONFIG) );
-			   
-			  
-			} 
-
-			http.onerror = function(error){
-				VIDEO_CONFIG = JSON.parse(localStorage.getItem( "properties" ) || "[]" );	 
-				 
-				 
+				VIDEO_CONFIG = JSON.parse(Base64.decode(http.responseText));
+				localStorage.setItem("properties", JSON.stringify(VIDEO_CONFIG));
 			}
+			http.onerror = function (error) {
+				VIDEO_CONFIG = JSON.parse(localStorage.getItem("properties") || "[]");
+			}
+		}
+		http.send();
+	} catch (err) {
+		alert(err);
 	}
-	http.send();
-}catch(err){alert(err);}
- 
- 
- 
- 
- }
-
-
+}
 
 loadPropertiesData();
-
-
